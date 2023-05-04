72807a77a7e08afbc5d01519a73807ebd3f82824
Fred Meziere <Fredzem@outlook.fr>
2023-05-04 16:54:04 +0200

    Controller User / Product

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -1,29 +1,146 @@
+const { Product } = require("../models");
+
 const productController = {
 
-    //Récuprer tous les étirements.
-    async getAllProducts(req, res) {
-        return res.json({ message:"Coucou, je suis un produit" });
+    async getAllProduct(req, res) {
 
-        // On récupère la liste des étirements depuis la DB à partir du model Sequelize
-        // try {
-        //     const products = await ;
-            //utilisation du .map pour mettre que les infos utilisés côté front
-            // const filtredProducts = products.map(etirement => {
-            //     return {
-            //         id : product.id,
-            //         title: product.title,
-            //         main_image: product.main_image,
-            //         categorie_id: product.category_id
-            //     };
-            // });
-            // res.status(200).json(filtredProducts);
+     
+        try {
+            const Product = await Stretch.findAll();
+           
+            const filtredProduct = Product.map(produit => {
+                return {
+                    id : produit.id,
+                    title: produit.title,
+                    description: produit.description,
+                    price: produit.price,
+                    price_reduce: produit.price_reduce,
+                    main_image: produit.main_image,
+                    guide_image: produit.guide_image,
+                    category_id: produit.category_id,
+                    image_id: produit.image_id,
+                    remaining_quantity: produit.remaining_quantity
+                };
+            });
+            res.status(200).json(filtredProduct);
 
-        // Permet de d'indique que le serveur a rencontré un problème inattendu qui l'empêche de répondre à la requête.         
-        // } catch (error) {
-        //     console.log(error);
-        //     return res.status(500).json({ error: "Internal server error" });
-        // }
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: "Internal server error" });
+        }
     },
-}
 
+    async getOneProduct(req, res) {
+        
+        try {
+            const productId = req.params.id;
+            const product = await Product.findByPk(productId);
+            res.status(200).json(product);
+
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: "Internal server error" });
+        }
+    },
+
+    async createProduct(req, res) {
+        try {
+            const {
+                title,
+                description, 
+                price,
+                price_reduce,
+                main_image, 
+                guide_image,
+                category_id,
+                image_id,
+                remaining_quantity
+            } = req.body;
+
+            const newProduct = await Product.create({ 
+                title,
+                description, 
+                price,
+                price_reduce,
+                main_image, 
+                guide_image,
+                category_id,
+                image_id,
+                remaining_quantity
+            }); 
+            
+            res.status(201).json({
+                stretch: {
+                    id: newProduct.id,
+                    title: newProduct.title,
+                    description: newProduct.description,
+                    price: newProduct.price,
+                    price_reduce: newProduct.price_reduce,
+                    main_image: newProduct.main_image,
+                    guide_image: newProduct.guide_image,
+                    category_id: newProduct.category_id,
+                    image_id: newProduct.image_id,
+                    remaining_quantity: newProduct.remaining_quantity
+                },
+            });
+
+    
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ errorMessage: "Erreur serveur" });
+        }
+    },
+
+    async updateProduct(req, res) {
+        const {title, description, price, price_reduce, main_image, guide_image, category_id, image_id, remaining_quantity} = req.body;
+        const productId = req.params.id;
+        const productToUpdate = await Stretch.findByPk(productId);
+
+        if (title !== undefined) {
+            productToUpdate.title = title;
+        }
+        if (description !== undefined) {
+            productToUpdate.description = description;
+        }
+        if (price !== undefined) {
+            productToUpdate.price = price;
+        }
+        if (price_reduce !== undefined) {
+            productToUpdate.price_reduce = price_reduce;
+        }
+        if (main_image !== undefined) {
+            productToUpdate.main_image = main_image;
+        }
+        if (guide_image !== undefined) {
+            productToUpdate.guide_image = guide_image;
+        }
+        if (category_id !== undefined) {
+            productToUpdate.category_id = category_id;
+        }
+        if (image_id !== undefined) {
+            productToUpdate.image_id = image_id;
+        }
+        if (remaining_quantity !== undefined) {
+            productToUpdate.remaining_quantity = remaining_quantity;
+        }
+        
+        await productToUpdate.save();
+
+        res.status(200).json();
+
+    },
+
+    async deleteProduct(req, res) {
+        const productId = req.params.id;
+        const productToDelete = await Product.findByPk(productId);
+        if (!productToDelete) {
+            return res.status(404).json({ error: "Product not found" });
+        }
+        await productToDelete.destroy();
+        res.status(200).json();
+    }
+
+
+
+};
 module.exports = productController;
--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,4 +1,203 @@
+const { User } = require("../models");
+
+const emailValidator = require("email-validator");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+async function hashPassword(password) {
+    // Hash du mot de passe pour qu'il ne soit plus en clair
+    const saltRounds = 20; // 20 représente le nombre de "round" de complexité pour générer le salt
+    // On génère d'abord un salt qui va permettre de faire en sorte que deux mdp identiques ne donnent pas le meme hash en sortie
+    const salt = await bcrypt.genSalt(saltRounds);
+    // Ensuite seulement on peut hasher le mdp avec le salt généré (qui sera du coup toujours différent et UNIQUE)
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+}
+
 const userController = {
 
-    async getAllUsers(req, res) {
+    //Formulaire d'inscription 
 
+
+    async signUpForm(req, res) {
+        const {
+            gender,
+            firstname,
+            lastname,
+            birthdate,
+            email,
+            emailconfirm,
+            password,
+            passwordconfirm,
+        } = req.body;
+
+
+        if (!gender || !firstname || !lastname || !birthdate || !email || !emailconfirm || !password || !passwordconfirm) {
+            return res.status(400).json({ error: "Veuillez entrer toutes les informations" });
+        }  
+        
+        if (emailValidator.validate(email)) {
+            return res.status(400).json({ error: "Veuillez entrer une adresse email valide" });
+        }
+
+        if (password !== passwordconfirm) {
+            return res.status(400).json({ error: "Les mots de passe ne correspondent pas" });
+        }
+
+        try {
+            const alreadyExistingUser = await User.findOne({
+                where: {
+                    email
+                }
+            });
+
+            if (alreadyExistingUser) {
+                return res.status(400).json({ errorMessage: `L'email ${email} est déjà utilisé` });
+            }
+
+            // Vérification du couple mdp / confirmation de mdp
+            if (password !== passwordConfirm) {
+                return res.status(400).json({ errorMessage: `La confirmation de mot de passe ne correspond pas au mot de passe renseigné` });
+            }
+            
+            const hashedPassword = await hashPassword(password);
+
+            const newUser = await User.create({
+                gender,
+                firstname,
+                lastname,
+                birthdate,
+                email,
+                password: hashedPassword
+            });
+
+            res.status(201).json({
+                user: {
+                    id: newUser.id,
+                    gender : newUser.gender,
+                    firstname: newUser.firstname,
+                    lastname: newUser.lastname,
+                    birthdate: newUser.birthdate,
+                    email: newUser.email,
+                },
+            });
+
+        } catch (error) {
+            console.log(error);
+
+            res.status(500).json({ errorMessage: "Erreur serveur" });
+        }
+    },
+
+    async loginFormSubmission(req, res) {
+        const { email , password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ error: "Veuillez entrer toutes les informations" });
+        }
+
+        try {
+            const existingUser = await User.findOne({
+                where: {
+                    email
+                }
+            });
+
+            if (!existingUser) {
+                return res.status(400).json({ errorMessage: "Email ou mot de passe incorrect" });
+            }
+
+            const hashedPassword = existingUser.password;
+ 
+            const passwordValidation = await bcrypt.compare(password, hashedPassword);
+
+            if (!passwordValidation) {
+                return res.status(400).json({ errorMessage: "Email ou mot de passe incorrect" });
+            }
+
+            const payload = {
+                sub: existingUser.id,
+                isAdmin: existingUser.role_id
+            };
+
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                expiresIn: "1h",
+            });
+ 
+            res.status(200).json({
+                message: "Connexion réussie !",
+                token
+            });
+
+        } catch (error) {
+            console.log(error);
+
+            res.status(500).json({ errorMessage: "Erreur serveur" });
+        }
+    },
+
+    logout(req, res) {
+        res.json({ message: "Utilisateur déconnecté" });
+    },
+
+    async getUserInfo(req, res) {
+        const userId = req.token.sub;
+       
+        const userFound = await User.findByPk(userId);
+        const { id, email, password, ...filtredUserInfo } = { id: userFound.id, email: userFound.email, password: userFound.password, firstname : userFound.firstname, lastnamme : userFound.lastname , birthdate : userFound.birthdate, role_id: userFound.role_id};
+        
+        res.json({ filtredUserInfo })
+    },
+
+    async updateUser(req, res) {
+        const { firstname, lastname, birthdate, email, address, additionnal_adress, zipcode, city} = req.body;
+
+        const userId = req.token.sub;
+        const userToUpdate = await User.findByPk(userId);
+
+        if (firstname !== undefined) { 
+            userToUpdate.username = username;
+        }
+        if (lastname!== undefined) { 
+            userToUpdate.lastname = lastname;
+        }
+        if (birthdate !== undefined) { 
+            userToUpdate.birthdate = birthdate;
+        }
+        if (email !== undefined) {
+            userToUpdate.email = email;
+        }   
+        if (address !== undefined) {
+            userToUpdate.address = address;
+        }
+        if (additionnal_adress !== undefined) {
+            userToUpdate.additionnal_adress = additionnal_adress;
+        }    
+        if (zipcode !== undefined) {
+            userToUpdate.zipcode = zipcode;
+        }
+        if (city !== undefined) {
+            userToUpdate.city = city;
+        }
+        
+        await userToUpdate.save();
+
+        res.status(204).end();
+    },
+
+    async deleteUser(req, res) {
+        
+        const userId = req.token.sub;
+        const userToDelete = await User.findByPk(userId);
+
+        if (!userToDelete) {
+            return res.status(404).json({ error: "Utilisateur non trouvé" });
+        }
+
+        await userToDelete.destroy();
+        res.status(204).end();
+    }
+}
+
+module.exports = userController;
